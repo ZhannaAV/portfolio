d4d30d0f04e9783ffffa5645c5ede1b46e44a94e
Zhanna Anikanova <whiteredfoxx@gmail.com>
2021-02-07 02:41:05 +0300

    feat:добавить фото и GSAP

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-// Get the iframe
-const iframe = document.getElementById('.project__iframe');
 // Reload the iframe
-iframe.contentWindow.location.reload();
+// const iframe = document.getElementById('.project__iframe');
+// iframe.contentWindow.location.reload();
+
+window.addEventListener('load', function () {
+    const aboutText = document.querySelector('.about')
+    TweenLite.to(aboutText, 4, {
+        opacity: 1,
+    })
+}, false);
